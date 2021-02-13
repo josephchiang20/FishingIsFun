6ee228313b3cdc314a4b3858a37c1b9a339414f0
joseph.chiang20 <joseph.chiang20@gmail.com>
2021-02-13 11:59:44 -0800

    add block site functionality

--- src/options.js
+++ src/options.js
@@ -4,17 +4,18 @@ const checkbox = document.getElementById("checkbox");
 
 save.addEventListener("click", () => {
   const blocked = textarea.value.split("\n").map(s => s.trim()).filter(Boolean);
-  chrome.storage.lobutt.set({ blocked });
+  console.log(blocked);
+  chrome.storage.local.set({ blocked });
 });
 
 checkbox.addEventListener("change", (event) => {
   const enabled = event.target.checked;
-  chrome.storage.lobutt.set({ enabled });
+  chrome.storage.local.set({ enabled });
 });
 
 window.addEventListener("DOMContentLoaded", () => {
-  chrome.storage.lobutt.get(["blocked", "enabled"], function (lobutt) {
-    const { blocked, enabled } = lobutt;
+  chrome.storage.local.get(["blocked", "enabled"], function (local) {
+    const { blocked, enabled } = local;
     if (Array.isArray(blocked)) {
       textarea.value = blocked.join("\n");
       checkbox.checked = enabled;
--- src/popup.js
+++ src/popup.js
@@ -6,15 +6,15 @@
  */
 
 
-document.addEventListener('DOMContentLoaded', function () {
-    var links = document.getElementsByTagName("a");
-    for (var i = 0; i < links.length; i++) {
-        (function () {
-            var ln = links[i];
-            var location = ln.href;
-            ln.onclick = function () {
-                chrome.tabs.create({active: true, url: location});
-            };
-        })();
-    }
-});
+// document.addEventListener('DOMContentLoaded', function () {
+//     var links = document.getElementsByTagName("a");
+//     for (var i = 0; i < links.length; i++) {
+//         (function () {
+//             var ln = links[i];
+//             var location = ln.href;
+//             ln.onclick = function () {
+//                 chrome.tabs.create({active: true, url: location});
+//             };
+//         })();
+//     }
+// });
