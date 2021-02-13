443c5d56395ba10f2cee726d61b2d1c66b09bdfb
QueenChristina <qinkristina@gmail.com>
2021-02-13 14:57:36 -0800

    added fishing.html

--- pet/fish.js
+++ pet/fish.js
@@ -1,16 +1,22 @@
 var images = document.getElementsByTagName('img');
+
 /*
 var div=document.createElement("div"); 
 document.body.appendChild(div); 
 div.innerText="test123";
 */
 
-
+/*
 var iFrame  = document.createElement ("iframe");
 iFrame.src  = chrome.extension.getURL ("/pet/fish.html");
 
 document.body.insertBefore (iFrame, document.body.firstChild);
+*/
 
+
+/*document.body.appendChild(iFrame);*/
+/*document.getElementsByTagName('footer')[0].appendChild(iFrame);*/
+/*document.querySelector('footer') = iFrame;*/
 /*
 $(document).ready (jQueryMain);
 
--- src/popup.js
+++ src/popup.js
@@ -3,3 +3,7 @@ const options = document.getElementById("options");
 options.addEventListener("click", () => {
     window.open("options.html", "_blank");
 });
+
+document.getElementById("pet").onclick = function () {
+    location.href = "fishing.html";
+};
