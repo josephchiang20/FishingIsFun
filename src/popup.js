44ee1580ef167e6a90ebb4042993be49dbbd677f
QueenChristina <qinkristina@gmail.com>
2021-02-13 20:39:36 -0800

    Stylistic change to settings, go fish, and back button

--- src/popup.js
+++ src/popup.js
@@ -1,9 +1,12 @@
 const options = document.getElementById("options");
 const fishing = document.getElementById("fishing");
 const cats = document.getElementById("cats");
+const back = document.getElementById("back");
 
 let motivational = ["You're fishing good.", "You're ameozing!", "You're reeling well!", "Meow! Moew!", "This window is fishy...", 
-"I wonder what the world is like beyond this window.", "Study with meow!", "We'll catch those fish!"];
+"I wonder what the world is like beyond this window.", "Study with meow!", "We'll catch those fish!", "Study on!", "Fish on!",
+"You're doing ameowzing!", "I'm proud of you, meow.", "Even though I'm stuck in here, I still believe in you.", "We're in this boat together.",
+"We'll get to the end of this ocean!", "The Cs are harsh today, arr!"];
 
 if (options != null) {
     options.addEventListener("click", () => {
@@ -19,5 +22,10 @@ if (cats != null) {
     cats.addEventListener("click", () => {
         var motivate = document.getElementById('motivate');
         motivate.textContent = motivational[Math.floor(Math.random() *motivational.length)]
+    });
+}
+if(back != null) {
+    back.addEventListener("click", () => {
+        location.href = "popup.html";
     });
 }
