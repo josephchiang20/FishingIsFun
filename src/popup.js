f1863a05aa08cf1b4f36d6c7b316ccbb088c2d28
joseph.chiang20 <joseph.chiang20@gmail.com>
2021-02-13 17:35:07 -0800

    rework event listeners

--- src/popup.js
+++ src/popup.js
@@ -1,10 +1,31 @@
 const options = document.getElementById("options");
 const fishing = document.getElementById("fishing");
+const cats = document.getElementById("cats");
 
-options.addEventListener("click", () => {
-    window.open("options.html", "_blank");
-});
+// options.addEventListener("click", () => {
+//     window.open("options.html", "_blank");
+// });
 
-fishing.addEventListener("click", () => {
-    location.href = "fishing.html";
-});
+// fishing.addEventListener("click", () => {
+//     location.href = "fishing.html";
+// });
+
+// cats.addEventListener("click", () => {
+//     alert("hello");
+// })
+
+if (options != null) {
+    options.addEventListener("click", () => {
+        window.open("options.html", "_blank");
+    });
+} 
+if (fishing != null) {
+    fishing.addEventListener("click", () => {
+        location.href = "fishing.html";
+    });
+} 
+if (cats != null) {
+    cats.addEventListener("click", () => {
+        alert("hello");
+    });
+}
